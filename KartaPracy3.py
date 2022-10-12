a6b52ba1541ac173b5062ca3da8099caf801bf9d
1-b 33 Y. R <114579172+Roddeeeon212@users.noreply.github.com>
2022-10-12 11:19:40 +0000

    zrobilem zad1

---
+++ KartaPracy3.py
@@ -0,0 +1,17 @@
+# Zad 1
+# for i in range(10, 22):
+#     print(i, end=" ")
+# zad 3
+# for i in range(99,9,-1):
+#    print(i, end=" ")
+
+# zad 4
+
+# for i in range(10,100,1):
+#     print(109-i, end=" ")
+
+# zad 5
+# for i in range(100,1000,20):  print(109-i, end=" ")
+
+
+
